75bbdb941871870c73b1dbc7b480c947a0df5061
imyroslav <imyroslav@ukr.net>
2025-03-23 23:01:45 +0100

    task-1-completed

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const getUsersWithFriend(users, friendName) => {
+
+    const userWithFriend = users.filter(user => 
+        
+        if(user.name.includesOf(friendName)) {
+        
+        }
+        
+
+    )
+
+}
